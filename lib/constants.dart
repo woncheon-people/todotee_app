2dd1fdd83407190f2379e224b78b65501532ea7b
onehunnitconst <skygana@gmail.com>
2023-10-28 17:02:02 +0900

    apiRoute 값 업데이트

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
 class Constants {
-  static const String apiRoute = 'http://10.0.2.2:8080';
+  static final String apiRoute =
+      Platform.isAndroid ? 'http://10.0.2.2:8080' : 'http://127.0.0.1:8080';
 }
